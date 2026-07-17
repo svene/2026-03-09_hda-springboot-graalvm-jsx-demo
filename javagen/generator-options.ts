4e526e93fa740d7a220942c37180682dcf83f967
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-07-17 06:55:18 +0200

    removed TS->Java generator

--- javagen/generator-options.ts
+++
@@ -1,8 +0,0 @@
-import { Project } from "ts-morph";
-
-export interface SharedConstGeneratorOptions {
-	project: Project;
-	inputGlob: string | string[];
-	outputDir: string;
-	javaPackage: string;
-}
